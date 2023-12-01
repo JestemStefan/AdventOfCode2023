fca23e60edc63f8b9c73722eed47378d38bcf5c0
Bartosz Stasiak <bstasiak@i2asolutions.com>
2023-12-01 20:20:34 +0100

    Add solution for part 2, but it's awful

--- Day_01/solution.py
+++ Day_01/solution.py
@@ -26,50 +26,61 @@ def solution_part_1():
     return solution
 
 
-@benchmark(1)
+@benchmark(1000)
 def solution_part_2():
-    solution = 0
+    words_to_int_mapping = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
 
-    words = [
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "zero",
-    ]
-
+    # with open("input_data.txt") as file:
     with open("input_data.txt") as file:
+        partial_solutions = []
         for line in file:
-            # scan the line and fine the first digit
             first_digit = None
-            for c in line:
-                if c.isdigit():
-                    first_digit = c
-                    break
-
-                if c in words:
-                    first_digit = words.index(c) + 1
-                    break
-
-            # scan the line backwards and find the last digit
             last_digit = None
-            for c in reversed(line):
+            last_digit_idx = -1
+            partial_string = ""
+
+            for idx, c in enumerate(line):
+                partial_string += c
+
                 if c.isdigit():
-                    last_digit = c
-                    break
+                    if first_digit is None:
+                        first_digit = int(c)
 
-                if c in words:
-                    last_digit = words.index(c) + 1
-                    break
+                    last_digit = int(c)
+                    last_digit_idx = idx
 
-            solution += int(first_digit + last_digit)
+                else:
+                    if len(partial_string) < 3:
+                        continue
 
-    return solution
+                    for word in words_to_int_mapping:
+                        if first_digit is None:
+                            l_words_idx = partial_string.find(word)
+                            if l_words_idx != -1:
+                                first_digit = words_to_int_mapping[word]
+
+                        r_words_idx = partial_string.rfind(word)
+
+                        if r_words_idx != -1 and r_words_idx > last_digit_idx:
+                            last_digit = words_to_int_mapping[word]
+                            last_digit_idx = r_words_idx
+
+            digit = first_digit * 10 + last_digit
+
+            partial_solutions.append(digit)
+
+    return sum(partial_solutions)
 
 
 if __name__ == "__main__":
